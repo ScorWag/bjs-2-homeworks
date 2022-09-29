8d6580df12d73806f55fdae1537445b394e4823e
ScorWag <scorwag@rambler.ru>
2022-09-29 22:07:11 +0300

    Add code for objects task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,43 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name,
+  this.gender = gender,
+  this.age = age
 }
+
+new Student("Antony", "male", 32);
+new Student("Jessica", "female", 25);
+new Student("Anastasia", "female", 19);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined) {
+    this.marks = [...marks];
+  } else {
+    this.marks.push(...marks);
+  }  
+}
+
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  if(this.marks === undefined) {
+    return 0;
+  } else {
+    return average = (this.marks.reduce((acc, item) => {return acc + item}, 0)) / this.marks.length;
+  }  
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;    
+}
