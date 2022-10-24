44a8548fdbf012bc28b293b83fd3e6b377103ca3
ScorWag <scorwag@rambler.ru>
2022-10-24 18:47:40 +0300

    Add code for decorators tasks

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,44 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+    const hash = args.join(',');
+    const objectInCache = cache.find((item) => item.hash === hash);
+    if(objectInCache) {
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;
+    }
+    
+    let result = func(...args);
+    cache.push({hash: hash, value: result});
+    if(cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {  
+  let timeoutId = null;
+  wrapper.allCount = 0;
+  wrapper.count = 0;    
+  function wrapper(...args) {
+    wrapper.allCount++;        
+    if(timeoutId === null) {
+      timeoutId = setTimeout(() => {        
+        func(...args);
+        wrapper.count++;
+      });        
+    } else {
+    clearTimeout(timeoutId);
+    timeoutId = null;
+    timeoutId = setTimeout(() => {      
+      func(...args);
+      wrapper.count++;
+      }, delay)
+    }    
+  }  
+  return wrapper;
 }
